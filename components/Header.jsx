383c20b19213b268782073d79f84a1a4bdd9017e
Kevin Lacroix <kevin.lacroix@hetic.net>
2022-10-12 15:13:41 +0200

    Update header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Header() {
+ const menuslink = [
+  {
+    anchor:'Work',
+  },
+  {
+    anchor:'About',
+  },
+  {
+    anchor:'Contact',
+  },
+]
+
+
   return (
     <header className='header'>
+      <Image
+        width={42}
+        height={39}
+        objectFit="cover"
+        src={'/Logo.svg'}
+        alt=""
+      />
+      
+      <ul className="header-menu">
+          <li className="header-menu__item">
+            <Link href="/works"><a>Works</a></Link>
+          </li>
+          <li className="header-menu__item">
+            <Link href="/about"><a>About</a></Link>
+          </li>
+          <li className="header-menu__item">
+            <Link href="/contact"><a>Contact</a></Link>
+          </li>
 
+      </ul>
         
     </header>
   )
